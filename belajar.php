51cad330288e2e62e791dfaaa2682a761994209c
Faizahfai28 <fsausan28@gmail.com>
2021-02-25 15:31:51 +0700

    belajar

---
+++ belajar.php
@@ -0,0 +1,21 @@
+<?php
+    //variabel //angka ga pake ('')
+    $nama = 'Faizah';
+    $angka = 28;
+
+    //print nama 
+    echo "nama saya :".$nama;
+    echo "nama saya :".$nama. "kelas berapa :".$angka;
+    echo "<br>";
+    echo $angka;
+    echo "<br>";
+
+    echo 'Assalamualaikum.."jumat"';
+    
+    echo "<h1>Hyy</h1>";
+
+    echo "dokumen :".$_SERVER["DOCUMENT_ROOT"];
+
+    echo "dokumen :".$_SERVER["DOCUMENT_SELF"];
+
+?>
